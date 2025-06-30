d32317b2074e1179c82aa4b45c89758d13d34050
Артём Каримов <grandbo@list.ru>
2025-06-30 22:14:18 +0500

    Добавьте файлы проекта.

---
+++ HomoEvo.py
@@ -0,0 +1,11 @@
+#austrolopithecus
+e1 = input("Введите 1 этап размития челеовека: ")
+#homo habilus
+e2 = input("Введите 2 этап размития челеовека: ")
+#homo erectus
+e3 = input("Введите 3 этап размития челеовека: ")
+#homo sapiens neanderthalensis
+e4 = input("Введите 4 этап размития челеовека: ")
+#homo sapiens sapiens
+e5 = input("Введите 5 этап размития челеовека: ")
+print(e1,e2,e3,e4,e5, sep='=>')
