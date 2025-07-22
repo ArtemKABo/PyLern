c70341b85b801a9efa559b460de38d0e0c91be6a
Артём Каримов <grandbo@list.ru>
2025-07-22 21:50:46 +0500

    Fix

--- Lern16/Turtle.py
+++ Lern16/Turtle.py
@@ -3,7 +3,6 @@ class turtle(object):
     __x = 0
     __y = 0
     __s = 1
-    __m = False #умеет ли черепаха сама управлять своей скоростью
 
     def __init__(self, x, y, s):
         self.__x = x
@@ -38,18 +37,37 @@ class turtle(object):
         self.__s -= 1
 
     # count_moves(x2, y2) - возвращает минимальное количество действий, за которое черепашка сможет добраться до x2 y2 от текущей позиции
-    def count_moves( x2, y2):
-        #Создаем ноды примерно так
-        
-	# public class Nodes
-	# {
-	# 	public int NodeNumber;
-	# 	public List<Nodes> IncidentNodes = new List<Nodes>();
-	# }
-
-        return 1
+    def count_moves(self, x2, y2):
+        fields = {}
+        fields[self.__y, self.__x] = { "s" : self.__s, "l" : 0, "nodes" : [] }
+        for i in range(self.__y, y2+1, 1 if y2 >= self.__y else -1):
+            for j in range(self.__y, y2+1, 1 if y2 >= self.__y else -1):
+                
 
 
+        l=1
+        return(l)
+
+    def compute_nodes()
+        #Создаем двумерный список условного поля
+        #в каждой ячейке сохраняем ноду
+        #Создаем ноды 
+            #хранит скорость 
+            #хранит список предыдущих ячеек
+            #Хранит вес
+        #Перебераем ячейки по вертикали и горизонтали ставим ноды
+        #Перебираем все ячейкм проставляя все ноды
+            # public class Nodes
+            # {
+            # 	public int NodeNumber;
+            # 	public List<Nodes> IncidentNodes = new List<Nodes>();
+            # }
+
+
+
+t = turtle(1,1,5)
+
+print(t.count_moves(10,4))
 
 
 
